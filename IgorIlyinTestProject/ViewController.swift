b686aa0cd35c44a0da5dd5d443874a2b4c03314f
Igor Ilyin <igordevmail@gmail.com>
2021-03-10 09:05:56 +0200

    Initial Commit

---
+++ IgorIlyinTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IgorIlyinTestProject
+//
+//  Created by Igor on 10.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
